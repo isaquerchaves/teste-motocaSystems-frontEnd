bd73379cfe7d0872ce07172384e94020c301ec67
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-14 10:02:07 -0300

    feat: add Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,25 @@
+import { CircleArrowUp, Plus } from "lucide-react";
+
+interface ButtonProps {
+  type: string;
+  text: string;
+  icon: string;
+}
+
+const Button = ({ type, text, icon }: ButtonProps) => {
+  if (type === "link")
+    return (
+      <a href="/criar" className="new-register">
+        <Plus className="icon" size={20} />
+        <p className="new-record">{text}</p>
+      </a>
+    );
+    return (
+      <button className="register" type="submit">
+        {icon === 'plus' ? <Plus className="icon" size={20} />: <CircleArrowUp className="icon" size={20} />}
+        <p className="new-record">{text}</p>
+      </button>
+    );
+};
+
+export default Button;
