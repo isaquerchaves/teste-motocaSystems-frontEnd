3442c500102c6b9dde39dff7a664e459cde86f28
Isaque Chaves <isaque.ramosch@gmail.com>
2024-06-13 22:46:38 -0300

    feat: add Search function

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,42 @@
-import React, { useState, useEffect } from 'react';
 import { Loader, Plus, Search } from "lucide-react";
 import Header from "../../components/Header/Header";
-import MotorcycleItem from "../../components/Motorcycle-Item/Motorcycle-Itens";
 import { Motorcycle, initialMotorcycle } from "../../services/services";
 import "./Home.css";
+import MotorcycleItem from "../../components/Motorcycle-Itens/Motorcycle-Itens";
+import { useState, useEffect } from "react";
 import { loadMotorcycle, deleteMotorcycle } from "../../helpers/localStorage";
 
 const Home = () => {
-  const [moto, setMoto] = useState<Motorcycle[]>([]);
+  const [motos, setMotos] = useState<Motorcycle[]>([]);
   const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     const storedMotos = loadMotorcycle();
     if (storedMotos) {
-      setMoto(storedMotos);
-      setLoading(false);
+      setMotos(storedMotos);
     } else {
-      // Definindo no local storage os dados do service, apenas na primeira execução
-      localStorage.setItem('motos', JSON.stringify(initialMotorcycle));
-      setMoto(initialMotorcycle);
-      setLoading(false);
+      localStorage.setItem("motos", JSON.stringify(initialMotorcycle));
+      setMotos(initialMotorcycle);
     }
+    setLoading(false);
   }, []);
 
   const handleDeleteMoto = (id: number) => {
     deleteMotorcycle(id);
-    setMoto(prevMotos => prevMotos.filter(m => m.id !== id));
+    setMotos((prevMotos) => prevMotos.filter((m) => m.id !== id));
+    alert("Registro deletado com sucesso!");
   };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredMotos = motos.filter((moto) =>
+    `${moto.code} ${moto.model} ${moto.color}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
 
   return (
     <section>
@@ -38,7 +48,12 @@ const Home = () => {
             <button>
               <Search size={20} />
             </button>
-            <input type="text" placeholder="Buscar por código, nome e cor" />
+            <input
+              type="text"
+              placeholder="Buscar por código, nome e cor"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
           </div>
           <a href="/criar" className="new-register">
             <Plus className="icon" size={20} />
@@ -57,12 +72,8 @@ const Home = () => {
         </div>
       ) : (
         <div className="moto-list">
-          {moto.map((motos, index) => (
-            <MotorcycleItem
-              key={index}
-              moto={motos}
-              onDelete={handleDeleteMoto}
-            />
+          {filteredMotos.map((moto) => (
+            <MotorcycleItem moto={moto} onDelete={handleDeleteMoto} />
           ))}
         </div>
       )}
